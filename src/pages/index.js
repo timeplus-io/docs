09d65aae673b0ee8202200972ecfadf6e97d1ac0
Jove Zhong <jove@timeplus.io>
2022-10-28 22:03:51 -0700

    switch font from Poppins to Inter

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import "@fontsource/poppins"; // Defaults to weight 400.
+import "@fontsource/inter"; // Defaults to weight 400.
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
