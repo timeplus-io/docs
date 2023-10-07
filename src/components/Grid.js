cf5400f1a4816f2f26b1e2f81ffa86f4f5b6eaa0
Jove Zhong <jove@timeplus.io>
2023-10-06 19:27:34 -0700

    fix the compile issue introduced by crowdin

---
+++ src/components/Grid.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from '@docusaurus/Link';
+import styles from './Grid.module.css';
+
+export default function Grid ({ children }) {
+  return (
+    <div className={styles.grid}>
+      {children}
+    </div>
+  );
+}
+
+export function GridItem ({ children, style = {}, href, title, cta }) {
+  return (
+    <>
+      <Link 
+        className={styles.button} 
+        style={style}
+        href={href}
+      >
+        {title && <h3>{title}</h3>}
+        {children && <p>{children}</p>}
+      </Link>
+    </>
+  );
+}
---
+++ src/components/Quickstart.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from '@docusaurus/Link';
+import styles from './Quickstart.module.css';
+
+export default function Grid ({ children, href }) {
+  return (
+    <Link href={href} className={styles.quickstart}>
+      <div className={styles.text}>
+        {children}
+        <span className={styles.cta}>Get started &rarr;</span>
+      </div>
+      <div className={styles.gif}>
+        <img src="/img/timeplus.gif" />
+      </div>
+    </Link>
+  );
+}
