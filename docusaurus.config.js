068145fa9456802bc30acc4dc38c04846ffe97b1
Jove Zhong <jove@timeplus.io>
2024-02-23 22:59:16 -0800

    auto format with zed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,48 +1,53 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const {themes} = require('prism-react-renderer');
+const { themes } = require("prism-react-renderer");
 const lightCodeTheme = themes.github;
 const darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Timeplus',
-  tagline: 'Fast + Powerful Real-time Analytics Made Intuitive',
-  url: 'https://docs.timeplus.com/',
-  baseUrl: '/',
-  onBrokenLinks: 'warn', // SHOULD BE throw
-  onBrokenAnchors: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'timeplus-io', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
-  scripts:['/hotjar.js','/ai.js',{src:'https://data.timeplus.com/p.js',async: true},{
-    // This script triggers page() on every page change. (URL Encoded)
-    src: "data:,let%20prevUrl%20%3D%20undefined%3B%0AsetInterval%28%28%29%20%3D%3E%20%7B%0A%20%20const%20currUrl%20%3D%20window.location.href%3B%0A%20%20if%20%28currUrl%20%21%3D%3D%20prevUrl%29%20%7B%0A%20%20%20%20%2F%2F%20URL%20changed%0A%20%20%20%20prevUrl%20%3D%20currUrl%3B%0A%20%20%20%20if%20%28window.jitsu%29%20%7B%0A%20%20%20%20%20%20window.jitsu.page%28%29%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%2C%201000%29%3B"
-  }],
-  plugins: ['docusaurus-plugin-hubspot'],
+  title: "Timeplus",
+  tagline: "Simple, powerful, cost-efficient stream processing",
+  url: "https://docs.timeplus.com/",
+  baseUrl: "/",
+  onBrokenLinks: "warn", // SHOULD BE throw
+  onBrokenAnchors: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "timeplus-io", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+  scripts: [
+    "/hotjar.js",
+    "/ai.js",
+    { src: "https://data.timeplus.com/p.js", async: true },
+    {
+      // This script triggers page() on every page change. (URL Encoded)
+      src: "data:,let%20prevUrl%20%3D%20undefined%3B%0AsetInterval%28%28%29%20%3D%3E%20%7B%0A%20%20const%20currUrl%20%3D%20window.location.href%3B%0A%20%20if%20%28currUrl%20%21%3D%3D%20prevUrl%29%20%7B%0A%20%20%20%20%2F%2F%20URL%20changed%0A%20%20%20%20prevUrl%20%3D%20currUrl%3B%0A%20%20%20%20if%20%28window.jitsu%29%20%7B%0A%20%20%20%20%20%20window.jitsu.page%28%29%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%2C%201000%29%3B",
+    },
+  ],
+  plugins: ["docusaurus-plugin-hubspot"],
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         gtag: {
-          trackingID: 'G-8HHYCJFNRD',
+          trackingID: "G-8HHYCJFNRD",
           anonymizeIP: true,
-        }, 
+        },
         docs: {
-          path: 'docs',
-          routeBasePath: '/', // Serve the docs at the site's root
-          remarkPlugins: [require('mdx-mermaid')],
-          sidebarPath: require.resolve('./sidebars.js'),
+          path: "docs",
+          routeBasePath: "/", // Serve the docs at the site's root
+          remarkPlugins: [require("mdx-mermaid")],
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           //editUrl: 'https://github.com/timeplus-io/docs/blob/main',
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -51,88 +56,88 @@ const config = {
   markdown: {
     mermaid: true,
   },
-  themes: ['@docusaurus/theme-mermaid'],
+  themes: ["@docusaurus/theme-mermaid"],
 
   themeConfig:
-    /** @type 
+    /** @type
      * {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       hubspot: {
         accountId: 23123537,
-      },     
-      mermaid:{
-        theme: {light: 'neutral', dark: 'dark'},
       },
-      docs:{
-        sidebar:{
+      mermaid: {
+        theme: { light: "neutral", dark: "dark" },
+      },
+      docs: {
+        sidebar: {
           hideable: true,
           autoCollapseCategories: true,
-        }
+        },
       },
       tableOfContents: {
         minHeadingLevel: 2,
         maxHeadingLevel: 6,
       },
       algolia: {
-        appId:'UCWO77T9MZ',
-        apiKey:'57b8ba245e7467472b18e6cbf5cfd384',
-        indexName:'public_docs',
+        appId: "UCWO77T9MZ",
+        apiKey: "57b8ba245e7467472b18e6cbf5cfd384",
+        indexName: "public_docs",
       },
       navbar: {
-        title: 'Docs',
+        title: "Docs",
         hideOnScroll: true,
         logo: {
-          alt: 'Timeplus',
-          src: 'img/Option1_B.png',
-          srcDark: 'img/Option1_W.png',
+          alt: "Timeplus",
+          src: "img/Option1_B.png",
+          srcDark: "img/Option1_W.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'Overview',
+            type: "doc",
+            docId: "index",
+            position: "left",
+            label: "Overview",
           },
           {
-            type: 'doc',
-            docId: 'quickstart',
-            position: 'left',
-            label: 'Quickstart',
+            type: "doc",
+            docId: "quickstart",
+            position: "left",
+            label: "Quickstart",
           },
           {
-            type: 'doc',
-            docId: 'functions',
-            position: 'left',
-            label: 'Functions',
+            type: "doc",
+            docId: "functions",
+            position: "left",
+            label: "Functions",
           },
           {
-            type: 'doc',
-            docId: 'changelog',
-            position: 'left',
+            type: "doc",
+            docId: "changelog",
+            position: "left",
             label: "What's New",
           },
           {
-            href: 'https://docs.timeplus.com/rest.html',
-            position: 'left',
-            label: 'REST API',
+            href: "https://docs.timeplus.com/rest.html",
+            position: "left",
+            label: "REST API",
           },
           {
-            type: 'search',
-            position: 'right',
+            type: "search",
+            position: "right",
           },
           {
-            href: 'https://github.com/timeplus-io/proton',
-            label: ' ',
-            position: 'right',
-            className: 'header-github-link',
-            'aria-label': 'GitHub repository',
+            href: "https://github.com/timeplus-io/proton",
+            label: " ",
+            position: "right",
+            className: "header-github-link",
+            "aria-label": "GitHub repository",
           },
           {
-            href: 'https://timeplus.com/slack',
-            label: ' ',
-            position: 'right',
-            className: 'header-slack-link',
-            'aria-label': 'Slack community',
+            href: "https://timeplus.com/slack",
+            label: " ",
+            position: "right",
+            className: "header-slack-link",
+            "aria-label": "Slack community",
           },
           // {
           //   type: 'localeDropdown',
@@ -141,39 +146,39 @@ const config = {
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Company',
+            title: "Company",
             items: [
               {
-                label: 'Company Home Page',
-                href: 'https://timeplus.com',
+                label: "Company Home Page",
+                href: "https://timeplus.com",
               },
               {
-                label: 'Timeplus Cloud',
-                href: 'https://us.timeplus.cloud',
+                label: "Timeplus Cloud",
+                href: "https://us.timeplus.cloud",
               },
               {
-                label: 'Live Demo',
-                href: 'https://demo.timeplus.cloud',
+                label: "Live Demo",
+                href: "https://demo.timeplus.cloud",
               },
               {
-                label: 'Blog',
-                href: 'https://timeplus.com/blog',
+                label: "Blog",
+                href: "https://timeplus.com/blog",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Community Slack',
-                href: 'https://timeplus.com/slack',
+                label: "Community Slack",
+                href: "https://timeplus.com/slack",
               },
               {
-                label: 'Github',
-                href: 'https://github.com/timeplus-io/proton',
+                label: "Github",
+                href: "https://github.com/timeplus-io/proton",
               },
             ],
           },
@@ -192,28 +197,36 @@ const config = {
             ],
           },*/
         ],
-        copyright: `Copyright © 2023 Timeplus, Inc.`,
+        copyright: `Copyright © 2024 Timeplus, Inc.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['java','json','hcl'],
+        additionalLanguages: [
+          "bash",
+          "hcl",
+          "java",
+          "javascript",
+          "json",
+          "python",
+          "sql",
+        ],
       },
-      colorMode:{
-        defaultMode:'dark',
+      colorMode: {
+        defaultMode: "dark",
         disableSwitch: true,
       },
     }),
   i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'zh'],
-      localeConfigs: {
-        zh: {
-          label: '中文',
-          htmlLang: 'zh-CN',
-        }
+    defaultLocale: "en",
+    locales: ["en", "zh"],
+    localeConfigs: {
+      zh: {
+        label: "中文",
+        htmlLang: "zh-CN",
       },
-    },  
+    },
+  },
 };
 
 module.exports = config;
