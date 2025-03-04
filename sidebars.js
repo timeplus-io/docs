17bd3387832bf81520da6caa85b06b5f01ccc7c9
Jove Zhong <jove@timeplus.io>
2025-03-03 17:23:01 -0800

    start working 2.8 doc

--- sidebars.js
+++ sidebars.js
@@ -521,6 +521,7 @@ const sidebars = {
         id: "release-notes",
       },
       items: [
+        "enterprise-v2.8",
         "enterprise-v2.7",
         "enterprise-v2.6",
         "enterprise-v2.5",
