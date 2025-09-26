51574882990460c4b0aa2b1efff66fc1b0751998
Wei <107260595+heyitswei@users.noreply.github.com>
2025-09-26 00:05:17 -0400

    Create googleanalytics.js

---
+++ static/googleanalytics.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KELVVD7S82"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KELVVD7S82');
+</script>
